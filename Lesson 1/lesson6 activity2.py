7880933b780d67a3fd303dcaa8708dba17ce6f6e
Penguin182017 <alek.famn@gmail.com>
2026-04-14 18:09:50 +0800

    lesson6 activity3

---
+++ Lesson 1/lesson6 activity2.py
@@ -0,0 +1,30 @@
+import turtle
+
+turtle.Screen().bgcolor("orange")
+turtle.color("blue")
+turtle.shape("turtle")
+board = turtle.Turtle()
+
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(50)
+
+board.pendown()
+board.right(90)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+turtle.done()
