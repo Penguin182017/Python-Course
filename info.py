c59c4be432f4fbd19ce4e02a1c805ba0adcc55ba
Penguin182017 <alek.famn@gmail.com>
2026-04-21 18:51:26 +0800

    info

---
+++ info.py
@@ -0,0 +1,11 @@
+personal_details = ("Alek", "Nihaj", 9, "142cm", "31.5kg", "Math")
+
+print("Original tuple:")
+print(personal_details)
+print("Type:", type(personal_details))
+
+personal_details_list = list(personal_details)
+
+print("\nConverted list:")
+print(personal_details_list)
+print("Type:", type(personal_details_list))
