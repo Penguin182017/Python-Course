762784aa3c20e56966dc3927a46c22d9a3f65d75
Alek <alek.famn@gmail.com>
2026-06-10 17:52:11 +0800

    lesson 23 activity2

---
+++ lesson23 activity2.py
@@ -0,0 +1,9 @@
+numberLargest = int(input("enter largest number: "))
+numberSmallest = int(input("enter smallest number: "))
+
+while(numberSmallest):
+    numberStore = numberSmallest
+    numberSmallest = numberLargest % numberSmallest
+    numberLargest = numberStore
+
+print("HCF is", numberLargest)
